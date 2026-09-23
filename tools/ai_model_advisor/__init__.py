a22d2618ca6d3dcc321c0b0a267c5f3d232e4b57
Mickey Vu <38831891+taiduc1302@users.noreply.github.com>
2026-09-23 10:49:49 -0700

    chore(advisor): bump version to v0.28.0

--- tools/ai_model_advisor/__init__.py
+++ tools/ai_model_advisor/__init__.py
@@ -9,6 +9,11 @@ from .hive_promotion_gate import build_hive_promotion_gate
 from .hive_promotion_preview import build_hive_promotion_preview
 from .hive_promotion_receipt import build_hive_promotion_receipt
 from .hive_promotion_rollback import build_hive_promotion_rollback_audit
+from .hive_promotion_journal import (
+    append_hive_promotion_journal,
+    build_hive_promotion_journal,
+    validate_hive_promotion_journal,
+)
 from .promotion_plan import build_promotion_plans
 from .promotion_review import build_promotion_review
 from .recommend import RecommendationEngine
@@ -24,6 +29,9 @@ __all__ = [
     "build_hive_promotion_preview",
     "build_hive_promotion_receipt",
     "build_hive_promotion_rollback_audit",
+    "append_hive_promotion_journal",
+    "build_hive_promotion_journal",
+    "validate_hive_promotion_journal",
     "ModelRegistry",
     "RecommendationEngine",
     "UsageRecord",
@@ -36,4 +44,4 @@ __all__ = [
     "evaluate_promotion_canary",
     "recommend_for_target",
 ]
-__version__ = "0.27.0"
+__version__ = "0.28.0"
